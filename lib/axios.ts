222c349a3956a4259f1645b63f211df8f46d5330
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-07 20:39:10 +0900

    ローカルのときもheroku叩く

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,12 +1,12 @@
 import Axios from "axios";
 
-// Axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+Axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 const production = process.env.NODE_ENV === "production";
 
 const axios = Axios.create({
   baseURL: production
     ? "https://yamoto-ir-api.herokuapp.com/"
-    : "http://localhost/",
+    : "https://yamoto-ir-api.herokuapp.com/",
 });
 
 export default axios;
