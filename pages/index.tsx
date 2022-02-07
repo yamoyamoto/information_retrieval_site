413325132306458ac1cc09fae689945dc14f5a2d
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-07 20:05:47 +0900

    カードをコンポーネントに

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,66 +3,51 @@ import Link from "next/link";
 import Card from "@material-ui/core/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { makeStyles } from "@mui/styles";
 
 const Home: NextPage = () => {
-  const cardStyle = {
-    height: "200px",
-    cursor: "pointer",
-  };
+  const workCardSettings: Array<Props> = [
+    {
+      title: "About Me",
+      description: "作成予定。",
+      href: "/",
+    },
+    {
+      title: "形態素解析ツール",
+      description: `日本語の文章を単語に分割するツール
+      (バックエンドはPython, フロントはNext.js)`,
+      href: "/information_retrieval",
+    },
+    {
+      title: "プチ検索エンジン",
+      description: `作成予定。`,
+      href: "/",
+    },
+    {
+      title: "簡易情報検索システム",
+      description: `作成予定。
+      (tf-idfやコサイン類似度による関連度計算の実装練習)`,
+      href: "/",
+    },
+  ];
+
+  let workCards = workCardSettings.map((setting) => {
+    return (
+      <WorkCard
+        title={setting.title}
+        description={setting.description}
+        href={setting.href}
+      ></WorkCard>
+    );
+  });
+
   return (
     <>
       <h1>Welcome to Yamoyamoto's Site !</h1>
 
       <div className="works_wrapper">
         <h2>Works</h2>
-        <div className="cards_wrapper">
-          <div className="card_wrapper">
-            <Link href="/information_retrieval">
-              <Card style={cardStyle}>
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    形態素解析ツール
-                  </Typography>
-                  <Typography variant="body2">
-                    日本語の文章を単語に分割するツール
-                    <br />
-                    <br />
-                    (バックエンドはPython, フロントはNext.js)
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Link>
-          </div>
-          <div className="card_wrapper">
-            <Link href="/">
-              <Card style={cardStyle}>
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    About Me
-                  </Typography>
-                  <Typography variant="body2">作成予定。</Typography>
-                </CardContent>
-              </Card>
-            </Link>
-          </div>
-          <div className="card_wrapper">
-            <Link href="/">
-              <Card style={cardStyle}>
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    簡易検索システム
-                  </Typography>
-                  <Typography variant="body2">
-                    作成予定。
-                    <br />
-                    <br />
-                    (tf-idfやコサイン類似度による関連度計算の実装練習)
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Link>
-          </div>
-        </div>
+        <div className="cards_wrapper">{workCards}</div>
       </div>
       <style jsx>{`
         .works_wrapper {
@@ -71,14 +56,48 @@ const Home: NextPage = () => {
 
         .cards_wrapper {
         }
-
-        .card_wrapper {
-          display: inline-block;
-          width: 250px;
-          margin: 10px;
-        }
       `}</style>
     </>
+  );
+};
+
+type Props = {
+  title: string;
+  description: string;
+  href: string;
+};
+
+const WorkCard: React.FC<Props> = (props: Props) => {
+  const useStyles = makeStyles({
+    card: {
+      "&:hover": { backgroundColor: "gray" },
+      height: "200px",
+      cursor: "pointer",
+    },
+  });
+
+  const style = `
+  .card_wrapper {
+    display: inline-block;
+    width: 250px;
+    margin: 10px;
+  }`;
+
+  const classes = useStyles();
+  return (
+    <div className="card_wrapper">
+      <Link href={props.href}>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {props.title}
+            </Typography>
+            <Typography variant="body2">{props.description}</Typography>
+          </CardContent>
+        </Card>
+      </Link>
+      <style jsx>{style}</style>
+    </div>
   );
 };
 
