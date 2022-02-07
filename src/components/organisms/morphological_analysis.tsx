2b1540068da9492de422a96c2c54ba450f4cd8fb
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-07 20:39:50 +0900

    style

--- src/components/organisms/morphological_analysis.tsx
+++ src/components/organisms/morphological_analysis.tsx
@@ -9,6 +9,7 @@ import {
   TableHead,
 } from "@mui/material";
 import axios from "../../../lib/axios";
+import { makeStyles } from "@mui/styles";
 
 type Props = {};
 
@@ -19,8 +20,8 @@ export const MorphologicalAnalyzer: React.FC<Props> = (props: Props) => {
   const execAnalysis = () => {
     const reqBody = {
       text: text,
-      use_word_class_filter: true,
-      word_classes: ["固有名詞"],
+      use_word_class_filter: false,
+      word_classes: [],
     };
     axios.post("/", reqBody).then((res) => {
       console.log(res);
@@ -28,31 +29,39 @@ export const MorphologicalAnalyzer: React.FC<Props> = (props: Props) => {
     });
   };
 
-  return (
-    <div
-      className="morphological_analysis_wrap"
-      style={{ minWidth: "50%", textAlign: "center" }}
-    >
-      <div>
-        <TextareaAutosize
-          aria-label="minimum height"
-          minRows={10}
-          placeholder="文章を入力"
-          style={{ width: "100%" }}
-          value={text}
-          onChange={(event) => {
-            setText(event.target.value);
-          }}
-        />
-      </div>
-      <Button variant="outlined" onClick={execAnalysis}>
-        解析する
-      </Button>
+  const styleJSX = `
+    .morphological_analysis_wrap{
+      text-align:center;
+      max-width:800px;
+      margin:auto;
+    }
+  `;
 
-      <div className="result_wrapper" style={{ margin: "50px 0" }}>
-        <ResultTable data={result} />
+  return (
+    <>
+      <div className="morphological_analysis_wrap">
+        <div>
+          <TextareaAutosize
+            aria-label="minimum height"
+            minRows={10}
+            placeholder="文章を入力"
+            style={{ width: "100%" }}
+            value={text}
+            onChange={(event) => {
+              setText(event.target.value);
+            }}
+          />
+        </div>
+        <Button variant="outlined" onClick={execAnalysis}>
+          解析する
+        </Button>
+
+        <div className="result_wrapper" style={{ margin: "50px 0" }}>
+          <ResultTable data={result} />
+        </div>
       </div>
-    </div>
+      <style jsx>{styleJSX}</style>
+    </>
   );
 };
 
@@ -72,34 +81,52 @@ type Morpheme = {
 };
 
 const ResultTable: React.FC<TableProps> = (props: TableProps) => {
-  console.log(props);
+  const useStyles = makeStyles({
+    tableCell: {
+      color: "white",
+    },
+  });
+  const classes = useStyles();
+
   return (
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-      <TableHead>
-        <TableRow>
-          <TableCell>Surface</TableCell>
-          <TableCell>WordClass</TableCell>
-          <TableCell>Count</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {props.data
-          ? props.data.map((rowData: RowData, i) => {
-              if (rowData.morpheme.surface != "") {
-                return (
-                  <TableRow
-                    key={i}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell>{rowData.morpheme.surface}</TableCell>
-                    <TableCell>{rowData.morpheme.featureString}</TableCell>
-                    <TableCell>{rowData.count}</TableCell>
-                  </TableRow>
-                );
-              }
-            })
-          : null}
-      </TableBody>
-    </Table>
+    <>
+      <div className="result_table_wrapper">
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.tableCell}>Surface</TableCell>
+              <TableCell className={classes.tableCell}>WordClass</TableCell>
+              <TableCell className={classes.tableCell}>Count</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.data
+              ? props.data.map((rowData: RowData, i) => {
+                  if (rowData.morpheme.surface != "") {
+                    return (
+                      <TableRow
+                        key={i}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell className={classes.tableCell}>
+                          {rowData.morpheme.surface}
+                        </TableCell>
+                        <TableCell className={classes.tableCell}>
+                          {rowData.morpheme.featureString}
+                        </TableCell>
+                        <TableCell className={classes.tableCell}>
+                          {rowData.count}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  }
+                })
+              : null}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   );
 };
