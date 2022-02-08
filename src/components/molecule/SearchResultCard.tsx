cbd293a5a37ba90e70107a9e9a9ac98af532f58c
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-08 13:10:47 +0900

    create SearchResultCard component

---
+++ src/components/molecule/SearchResultCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Card from "@material-ui/core/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { makeStyles } from "@mui/styles";
+
+export type SearchResultCardProps = {
+  title: string;
+  url: string;
+  metaDesc: string;
+};
+
+export const SearchResultCard = (props: SearchResultCardProps) => {
+  const useStyle = makeStyles({
+    cardWrapper: {
+      margin: "20px 0",
+      maxWidth: "600px",
+    },
+    searchResultWrapper: {
+      marginTop: "30px",
+    },
+  });
+  const classes = useStyle();
+
+  return (
+    <div className={classes.cardWrapper}>
+      <Card>
+        <CardContent>
+          <Typography variant="h5">
+            <a target="_blank" rel="noopener noreferrer" href={props.url}>
+              {props.title}
+            </a>
+          </Typography>
+          <Typography variant="body2">{props.metaDesc}</Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
