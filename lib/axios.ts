86065206f63dd04b56fb79224e8d0eaa48bcfd0e
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-10 17:28:44 +0900

    X-Forwarded-Proto

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,12 +1,11 @@
 import Axios from "axios";
 
 Axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+Axios.defaults.headers.get["X-Forwarded-Proto"] = "https";
 const production = process.env.NODE_ENV === "production";
 
 const axios = Axios.create({
-  baseURL: production
-    ? "https://yamoto-ir-api.herokuapp.com/"
-    : "https://yamoto-ir-api.herokuapp.com/",
+  baseURL: production ? "https://yamoto-ir-api.herokuapp.com/" : "https://yamoto-ir-api.herokuapp.com/",
 });
 
 export default axios;
