7a11d7a5dd804252c8c1c257662b03dbd0e42a68
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-10 17:04:11 +0900

    pagenation

--- src/components/organisms/MiniSearchEngine.tsx
+++ src/components/organisms/MiniSearchEngine.tsx
@@ -2,35 +2,34 @@ import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import { SearchBox } from "../molecule/SearchBox";
+import { Pagination, PaginationItem } from "@mui/material";
 
 import { SearchResultCard, SearchResultCardProps } from "../molecule/SearchResultCard";
-import axios from "../../../lib/googleApiAxios";
+import axios from "../../../lib/axios";
 
 type MiniSearchEngineProps = {};
 
-const apiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
-const searchEngineId = process.env.NEXT_PUBLIC_GOOGLE_SEARCH_ENGINE_ID;
-
 export const MiniSearchEngine = (props: MiniSearchEngineProps) => {
   const [query, setQuery] = React.useState("");
-  const [searchResults, updateSearchResults] = React.useState<SearchResultCardProps[]>([
-    {
-      title: "検索結果1",
-      url: "#",
-      metaDesc: "~~~~",
-    },
-  ]);
+  const [searchResults, updateSearchResults] = React.useState<SearchResultCardProps[]>([]);
+  const [nowPage, setNowPage] = React.useState<number>(0);
+  const [didFirstSearch, setDidFirstSearch] = React.useState(false);
 
   const useStyle = makeStyles({
     searchBoxWrapper: {
       maxWidth: "400px",
       display: "flex",
     },
+    paginationItem: {
+      color: "white",
+      borderColor: "gray",
+    },
   });
   const classes = useStyle();
 
-  const Search = (q: string) => {
-    axios.get(`/customsearch/v1?key=${apiKey}&cx=${searchEngineId}&q=${q}`).then((res) => {
+  const Search = (q: string, startIndex?: number) => {
+    setDidFirstSearch(true);
+    axios.get(`/search?q=${q}&start_index=${startIndex || 1}`).then((res) => {
       try {
         console.log(res);
         let newSearchResults = res.data.items.map((item: any) => {
@@ -51,12 +50,37 @@ export const MiniSearchEngine = (props: MiniSearchEngineProps) => {
     return <SearchResultCard key={i} {...props} />;
   });
 
+  const changePage = (event: React.ChangeEvent<unknown>, page: number) => {
+    setNowPage(page);
+  };
+  React.useEffect(() => {
+    if (didFirstSearch) {
+      Search(query, 10 * nowPage + 1);
+    }
+  }, [nowPage]);
   return (
     <div className="mini_search_engine_wrapper">
       <div className={classes.searchBoxWrapper}>
         <SearchBox query={query} updateQuery={setQuery} onEnterButton={Search} />
       </div>
       <div className="search_result_wrapper">{searchResultCards}</div>
+      <div className="nav_bottom">
+        <Pagination
+          page={nowPage}
+          onChange={changePage}
+          variant="outlined"
+          count={9}
+          renderItem={(item) => {
+            return (
+              <PaginationItem
+                className={classes.paginationItem}
+                style={{ borderColor: item.selected ? "white" : "black" }}
+                {...item}
+              />
+            );
+          }}
+        />
+      </div>
     </div>
   );
 };
