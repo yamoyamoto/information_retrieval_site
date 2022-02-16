6d94f5ec3b9aaa3ea185f5906598b9e20e3130d6
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-16 10:57:58 +0900

    WIP

---
+++ src/components/molecule/DocumentCard.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { makeStyles } from "@material-ui/core";
+
+
+type DocumentCardProps = {
+  body: string;
+  tf?: number;
+  df?: number;
+  idf?: number;
+  tfIdf?: number;
+}
+
+export const DocumentCard = (props: DocumentCardProps) => {
+  const classes = useStyle();
+
+  return (
+    <div className={classes.cardWrapper}>
+      <p>{props.body}</p>
+      <p>tf:{props.tf || "??"}</p>
+      <p>df:{props.df || "??"}</p>
+      <p>idf:{props.idf || "??"}</p>
+      <p>tf-idf:{props.tfIdf || "??"}</p>
+    </div>
+  );
+}
+
+const useStyle = makeStyles({
+  cardWrapper: {
+    marginTop: "20px",
+    maxWidth: "600px",
+    backgroundColor: "white",
+    color: "black",
+  },
+});
--- src/components/organisms/SearchDocumentApp.tsx
+++ src/components/organisms/SearchDocumentApp.tsx
@@ -2,17 +2,52 @@ import * as React from "react";
 import { makeStyles } from "@material-ui/core";
 
 import { SearchBox } from "../molecule/SearchBox";
+import { DocumentCard } from "../molecule/DocumentCard";
+import axios from "../../../lib/axios";
 
 type SearchDocumentAppProps = {};
 
 export const SearchDocumentApp = (props: SearchDocumentAppProps) => {
   const [query, setQuery] = React.useState("");
+  const [resultCards, setResultCards] = React.useState([]);
 
   const classes = useStyle();
+
+  const Search = (q: string) => {
+    const reqBody = {
+      q: q,
+    }
+    axios.post("document/search/tf_idf", reqBody).then((res) => {
+      console.log(res);
+      try {
+        const cards = res.data.result.map((one, i: number) => {
+          return (
+            <DocumentCard
+              key={i}
+              body={one.document.body}
+              tf={one.tf}
+              df={one.df}
+              idf={one.idf}
+              tfIdf={one.tfIdf}
+            />
+          );
+        });
+        setResultCards(cards);
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  }
+
   return (
-    <div className={classes.searchBoxWrapper}>
-      <SearchBox query={query} updateQuery={setQuery} onEnterButton={(q: string) => { }} />
-    </div>
+    <>
+      <div className={classes.searchBoxWrapper}>
+        <SearchBox query={query} updateQuery={setQuery} onEnterButton={Search} />
+      </div>
+      <div >
+        {resultCards}
+      </div>
+    </>
   );
 };
 
@@ -20,9 +55,5 @@ const useStyle = makeStyles({
   searchBoxWrapper: {
     maxWidth: "400px",
     display: "flex",
-  },
-  paginationItem: {
-    color: "white",
-    borderColor: "gray",
   },
 });
