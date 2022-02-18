feda2cb12496556a5bca8022e90b30ac19249b82
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-18 11:09:18 +0900

    fix import

--- src/components/molecule/SearchBox.tsx
+++ src/components/molecule/SearchBox.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles } from "@material-ui/core";
 import { TextField, Button } from "@mui/material";
 
 const useStyle = makeStyles({
